329717adfb91383ac6374915a67b96a896aaa77e
Alexey Larin <alexey_larin@hotmail.com>
2023-05-01 17:00:49 +0300

    feat: add key highlight on press, language change, CapsLock and Shift keyboard keys change

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ const keys = [
     { id: 'Backquote', en: { text: '`', textShift: '~', textCaps: '`' }, ru: { text: 'ё', textShift: 'Ё', textCaps: 'Ё' } },
     { id: 'Digit1', en: { text: '1', textShift: '!', textCaps: '1' }, ru: { text: '1', textShift: '!', textCaps: '1' } },
     { id: 'Digit2', en: { text: '2', textShift: '@', textCaps: '2' }, ru: { text: '2', textShift: '"', textCaps: '2' } },
-    { id: 'Digit3', en: { text: '3', textShift: '#', textCaps: '`' }, ru: { text: '3', textShift: '№', textCaps: '3' } },
-    { id: 'Digit4', en: { text: '4', textShift: '$', textCaps: '`' }, ru: { text: '4', textShift: ';', textCaps: '4' } },
-    { id: 'Digit5', en: { text: '5', textShift: '%', textCaps: '`' }, ru: { text: '5', textShift: '%', textCaps: '5' } },
-    { id: 'Digit6', en: { text: '6', textShift: '^', textCaps: '`' }, ru: { text: '6', textShift: ':', textCaps: '6' } },
-    { id: 'Digit7', en: { text: '7', textShift: '&', textCaps: '`' }, ru: { text: '7', textShift: '?', textCaps: '7' } },
-    { id: 'Digit8', en: { text: '8', textShift: '*', textCaps: '`' }, ru: { text: '8', textShift: '*', textCaps: '8' } },
-    { id: 'Digit9', en: { text: '9', textShift: '(', textCaps: '`' }, ru: { text: '9', textShift: '(', textCaps: '9' } },
-    { id: 'Digit0', en: { text: '0', textShift: ')', textCaps: '`' }, ru: { text: '0', textShift: ')', textCaps: '0' } },
-    { id: 'Minus', en: { text: '-', textShift: '_', textCaps: '`' }, ru: { text: '-', textShift: '_', textCaps: '-' } },
-    { id: 'Equal', en: { text: '=', textShift: '+', textCaps: '`' }, ru: { text: '=', textShift: '+', textCaps: '=' } },
+    { id: 'Digit3', en: { text: '3', textShift: '#', textCaps: '3' }, ru: { text: '3', textShift: '№', textCaps: '3' } },
+    { id: 'Digit4', en: { text: '4', textShift: '$', textCaps: '4' }, ru: { text: '4', textShift: ';', textCaps: '4' } },
+    { id: 'Digit5', en: { text: '5', textShift: '%', textCaps: '5' }, ru: { text: '5', textShift: '%', textCaps: '5' } },
+    { id: 'Digit6', en: { text: '6', textShift: '^', textCaps: '6' }, ru: { text: '6', textShift: ':', textCaps: '6' } },
+    { id: 'Digit7', en: { text: '7', textShift: '&', textCaps: '7' }, ru: { text: '7', textShift: '?', textCaps: '7' } },
+    { id: 'Digit8', en: { text: '8', textShift: '*', textCaps: '8' }, ru: { text: '8', textShift: '*', textCaps: '8' } },
+    { id: 'Digit9', en: { text: '9', textShift: '(', textCaps: '9' }, ru: { text: '9', textShift: '(', textCaps: '9' } },
+    { id: 'Digit0', en: { text: '0', textShift: ')', textCaps: '0' }, ru: { text: '0', textShift: ')', textCaps: '0' } },
+    { id: 'Minus', en: { text: '-', textShift: '_', textCaps: '-' }, ru: { text: '-', textShift: '_', textCaps: '-' } },
+    { id: 'Equal', en: { text: '=', textShift: '+', textCaps: '=' }, ru: { text: '=', textShift: '+', textCaps: '=' } },
     { id: 'Backspace', en: { text: 'Backspace', textShift: 'Backspace', textCaps: 'Backspace' }, ru: { text: 'Backspace', textShift: 'Backspace', textCaps: 'Backspace' } },
   ],
   [
@@ -44,22 +44,22 @@ const keys = [
     { id: 'KeyK', en: { text: 'k', textShift: 'K', textCaps: 'K' }, ru: { text: 'л', textShift: 'Л', textCaps: 'Л' } },
     { id: 'KeyL', en: { text: 'l', textShift: 'L', textCaps: 'L' }, ru: { text: 'д', textShift: 'Д', textCaps: 'Д' } },
     { id: 'Semicolon', en: { text: ';', textShift: ':', textCaps: ';' }, ru: { text: 'ж', textShift: 'Ж', textCaps: 'Ж' } },
-    { id: 'Quote', en: { text: "'", textShift: '"', textCaps: "'" }, ru: { text: 'э', textShift: 'Э', textCaps: 'Э' } },
+    { id: 'Quote', en: { text: '\'', textShift: '"', textCaps: '\'' }, ru: { text: 'э', textShift: 'Э', textCaps: 'Э' } },
     { id: 'Enter', en: { text: 'Enter', textShift: 'Enter', textCaps: 'Enter' }, ru: { text: 'Enter', textShift: 'Enter', textCaps: 'Enter' } },
   ],
   [
     { id: 'ShiftLeft', en: { text: 'Shift', textShift: 'Shift', textCaps: 'Shift' }, ru: { text: 'Shift', textShift: 'Shift', textCaps: 'Shift' } },
-    { id: 'KeyZ', en: { text: 'z', textShift: 'A', textCaps: 'A' }, ru: { text: 'ф', textShift: 'Ф', textCaps: 'Ф' } },
-    { id: 'KeyX', en: { text: 'x', textShift: 'S', textCaps: 'S' }, ru: { text: 'ы', textShift: 'Ы', textCaps: 'Ы' } },
-    { id: 'KeyC', en: { text: 'c', textShift: 'D', textCaps: 'D' }, ru: { text: 'в', textShift: 'В', textCaps: 'В' } },
-    { id: 'KeyV', en: { text: 'v', textShift: 'F', textCaps: 'F' }, ru: { text: 'а', textShift: 'А', textCaps: 'А' } },
-    { id: 'KeyB', en: { text: 'b', textShift: 'G', textCaps: 'G' }, ru: { text: 'п', textShift: 'П', textCaps: 'П' } },
-    { id: 'KeyN', en: { text: 'n', textShift: 'H', textCaps: 'H' }, ru: { text: 'р', textShift: 'Р', textCaps: 'Р' } },
-    { id: 'KeyM', en: { text: 'm', textShift: 'J', textCaps: 'J' }, ru: { text: 'о', textShift: 'О', textCaps: 'О' } },
-    { id: 'Comma', en: { text: ',', textShift: 'K', textCaps: 'K' }, ru: { text: 'л', textShift: 'Л', textCaps: 'Л' } },
-    { id: 'Period', en: { text: '.', textShift: 'L', textCaps: 'L' }, ru: { text: 'д', textShift: 'Д', textCaps: 'Д' } },
-    { id: 'Slash', en: { text: '/', textShift: ':', textCaps: ';' }, ru: { text: 'ж', textShift: 'Ж', textCaps: 'Ж' } },
-    { id: 'ArrowUp', en: { text: '▲', textShift: '▲', textCaps: '▲' }, ru: { text: '▲', textShift: '▲', textCaps: '▲' } },
+    { id: 'KeyZ', en: { text: 'z', textShift: 'Z', textCaps: 'Z' }, ru: { text: 'я', textShift: 'Я', textCaps: 'Я' } },
+    { id: 'KeyX', en: { text: 'x', textShift: 'X', textCaps: 'X' }, ru: { text: 'ч', textShift: 'Ч', textCaps: 'Ч' } },
+    { id: 'KeyC', en: { text: 'c', textShift: 'C', textCaps: 'C' }, ru: { text: 'с', textShift: 'С', textCaps: 'С' } },
+    { id: 'KeyV', en: { text: 'v', textShift: 'V', textCaps: 'V' }, ru: { text: 'м', textShift: 'М', textCaps: 'М' } },
+    { id: 'KeyB', en: { text: 'b', textShift: 'B', textCaps: 'B' }, ru: { text: 'и', textShift: 'И', textCaps: 'И' } },
+    { id: 'KeyN', en: { text: 'n', textShift: 'N', textCaps: 'N' }, ru: { text: 'т', textShift: 'Т', textCaps: 'Т' } },
+    { id: 'KeyM', en: { text: 'm', textShift: 'M', textCaps: 'M' }, ru: { text: 'ь', textShift: 'Ь', textCaps: 'Ь' } },
+    { id: 'Comma', en: { text: ',', textShift: '<', textCaps: ',' }, ru: { text: 'б', textShift: 'Б', textCaps: 'Б' } },
+    { id: 'Period', en: { text: '.', textShift: '>', textCaps: '.' }, ru: { text: 'ю', textShift: 'Ю', textCaps: 'Ю' } },
+    { id: 'Slash', en: { text: '/', textShift: '?', textCaps: '/' }, ru: { text: '.', textShift: ',', textCaps: '.' } },
+    { id: 'ArrowUp', en: { text: '⯅', textShift: '⯅', textCaps: '⯅' }, ru: { text: '⯅', textShift: '⯅', textCaps: '⯅' } },
     { id: 'ShiftRight', en: { text: 'Shift', textShift: 'Shift', textCaps: 'Shift' }, ru: { text: 'Shift', textShift: 'Shift', textCaps: 'Shift' } },
   ],
   [
@@ -68,12 +68,106 @@ const keys = [
     { id: 'AltLeft', en: { text: 'Alt', textShift: 'Alt', textCaps: 'Alt' }, ru: { text: 'Alt', textShift: 'Alt', textCaps: 'Alt' } },
     { id: 'Space', en: { text: 'Space', textShift: 'Space', textCaps: 'Space' }, ru: { text: 'Space', textShift: 'Space', textCaps: 'Space' } },
     { id: 'AltRight', en: { text: 'Alt', textShift: 'Alt', textCaps: 'Alt' }, ru: { text: 'Alt', textShift: 'Alt', textCaps: 'Alt' } },
-    { id: 'ArrowLeft', en: { text: '◀', textShift: '◀', textCaps: '◀' }, ru: { text: '◀', textShift: '◀', textCaps: '◀' } },
-    { id: 'ArrowDown', en: { text: '▼', textShift: '▼', textCaps: '▼' }, ru: { text: '▼', textShift: '▼', textCaps: '▼' } },
-    { id: 'ArrowRight', en: { text: '▶', textShift: '▶', textCaps: '▶' }, ru: { text: '▶', textShift: '▶', textCaps: '▶' } },
+    { id: 'ArrowLeft', en: { text: '⯇', textShift: '⯇', textCaps: '⯇' }, ru: { text: '⯇', textShift: '⯇', textCaps: '⯇' } },
+    { id: 'ArrowDown', en: { text: '⯆', textShift: '⯆', textCaps: '⯆' }, ru: { text: '⯆', textShift: '⯆', textCaps: '⯆' } },
+    { id: 'ArrowRight', en: { text: '⯈', textShift: '⯈', textCaps: '⯈' }, ru: { text: '⯈', textShift: '⯈', textCaps: '⯈' } },
     { id: 'ControlRight', en: { text: 'Ctrl', textShift: 'Ctrl', textCaps: 'Ctrl' }, ru: { text: 'Ctrl', textShift: 'Ctrl', textCaps: 'Ctrl' } },
   ],
 ];
+
+let currentLanguage = 'en';
+let isShiftPressed = false;
+let isCtrlPressed = false;
+let isCapsLockOn = false;
+
+// Update keyboard keys when CapsLock is on or Shift is pressed
+const updateKeys = () => {
+  keys.forEach((row) => {
+    row.forEach((key) => {
+      const keyElement = document.querySelector(`button[id="${key.id}"]`);
+      if (isCapsLockOn) {
+        keyElement.innerText = key[currentLanguage].textCaps;
+      } else if (isShiftPressed) {
+        keyElement.innerText = key[currentLanguage].textShift;
+      } else {
+        keyElement.innerText = key[currentLanguage].text;
+      }
+    });
+  });
+};
+
+window.addEventListener('keydown', (event) => {
+  if (event.code === 'CapsLock') {
+    isCapsLockOn = !isCapsLockOn;
+    updateKeys();
+  } else if (event.code === 'ShiftLeft' || event.code === 'ShiftRight') {
+    isShiftPressed = true;
+    updateKeys();
+  }
+});
+
+window.addEventListener('keyup', (event) => {
+  if (event.code === 'ShiftLeft' || event.code === 'ShiftRight') {
+    isShiftPressed = false;
+    updateKeys();
+  }
+});
+
+window.addEventListener('keypress', (event) => {
+  const capsLockKeyElement = document.querySelector('button[id="CapsLock"]');
+  if (event.getModifierState('CapsLock') !== isCapsLockOn) {
+    isCapsLockOn = !isCapsLockOn;
+    capsLockKeyElement.classList.toggle('active');
+  }
+  console.log(event.getModifierState('CapsLock'));
+});
+
+// Language change
+const changeLanguage = () => {
+  currentLanguage = currentLanguage === 'en' ? 'ru' : 'en';
+  keys.forEach((row) => {
+    row.forEach((key) => {
+      const keyElement = document.querySelector(`button[id="${key.id}"]`);
+      keyElement.innerText = key[currentLanguage].text;
+    });
+  });
+};
+
+window.addEventListener('keydown', (event) => {
+  if (event.code === 'ShiftLeft' || event.code === 'ShiftRight') {
+    isShiftPressed = true;
+  }
+  if (event.code === 'ControlLeft' || event.code === 'ControlRight') {
+    isCtrlPressed = true;
+  }
+  if (isShiftPressed && isCtrlPressed) {
+    changeLanguage();
+  }
+});
+
+window.addEventListener('keyup', (event) => {
+  if (event.code === 'ShiftLeft' || event.code === 'ShiftRight') {
+    isShiftPressed = false;
+  }
+  if (event.code === 'ControlLeft' || event.code === 'ControlRight') {
+    isCtrlPressed = false;
+  }
+});
+
+// Highlight keys when pressed on keyboard
+window.addEventListener('keydown', (event) => {
+  const keyElement = document.querySelector(`button[id="${event.code}"]`);
+  if (keyElement) {
+    keyElement.classList.add('active');
+  }
+});
+
+window.addEventListener('keyup', (event) => {
+  const keyElement = document.querySelector(`button[id="${event.code}"]`);
+  if (keyElement) {
+    keyElement.classList.remove('active');
+  }
+});
 
 window.onload = () => {
   // Create wrapper div
@@ -85,6 +179,7 @@ window.onload = () => {
   const textarea = document.createElement('textarea');
   textarea.classList.add('textarea');
   wrapper.appendChild(textarea);
+  textarea.focus();
 
   // Create keyboard div
   const keyboard = document.createElement('div');
@@ -106,6 +201,7 @@ window.onload = () => {
     row.forEach((key) => {
       const keyElement = document.createElement('button');
       keyElement.classList.add('key');
+      keyElement.setAttribute('id', key.id);
 
       switch (key.id) {
         case 'Backspace':
